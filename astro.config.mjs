f741f985076f2396d8efdc4d708e5b5ae905e3ab
DESKTOP-N1J8QTI\red <admin@redacks.com>
2024-04-28 14:03:45 +0200

    Adding Basic Project Structure and first Content

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,9 +3,10 @@ import starlight from "@astrojs/starlight";
 
 // https://astro.build/config
 export default defineConfig({
+  site: process.env.CI ? "https://redacks.github.io" : "http://localhost:4321",
+  base: process.env.CI ? "/hetzner-server" : undefined,
   integrations: [
     starlight({
-      base: "hetzner-server",
       title: "Hetzner-Server",
       social: {
         github: "https://github.com/Redacks/hetzner-server",
