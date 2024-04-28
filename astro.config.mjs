ef8d1b83dc32eaa93425f70445ce7505194887cd
DESKTOP-N1J8QTI\red <admin@redacks.com>
2024-04-28 13:21:26 +0200

    Setting up Starlight Basics

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,27 +1,21 @@
-import { defineConfig } from 'astro/config';
-import starlight from '@astrojs/starlight';
+import { defineConfig } from "astro/config";
+import starlight from "@astrojs/starlight";
 
 // https://astro.build/config
 export default defineConfig({
-	integrations: [
-		starlight({
-			title: 'My Docs',
-			social: {
-				github: 'https://github.com/withastro/starlight',
-			},
-			sidebar: [
-				{
-					label: 'Guides',
-					items: [
-						// Each item here is one entry in the navigation menu.
-						{ label: 'Example Guide', link: '/guides/example/' },
-					],
-				},
-				{
-					label: 'Reference',
-					autogenerate: { directory: 'reference' },
-				},
-			],
-		}),
-	],
+  integrations: [
+    starlight({
+      base: "hetzner-server",
+      title: "Hetzner-Server",
+      social: {
+        github: "https://github.com/Redacks/hetzner-server",
+      },
+      sidebar: [
+        {
+          label: "Guides",
+          autogenerate: { directory: "guides" },
+        },
+      ],
+    }),
+  ],
 });
